ee3f8a4a4533c7a003187d542d50ddcc98a5b479
Matias Falcone <matias.falcone@gmail.com>
2024-04-19 13:37:01 +1000

    added chainlink automation to raffle contract

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -14,6 +14,11 @@ contract Raffle is VRFConsumerBaseV2 {
     error Raffle__NotEnoughEthSent();
     error Raffle__TransferFailed();
     error Raffle__RaffleNotOpen();
+    error Raffle__UpkeepNotNeeded(
+        RaffleState raffleState,
+        uint256 currentBalance,
+        uint256 numPlayers
+    );
 
     /** Type Declarations */
     enum RaffleState {
@@ -72,7 +77,35 @@ contract Raffle is VRFConsumerBaseV2 {
         emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() external {
+    /**
+     * @dev This is the function that the Chainlink Automation nodes call
+     * to see if it's time to perform an upkeep.
+     * The following should be true for this to return true:
+     * 1. The time interval has passed between raffle runs
+     * 2. The raffle is in the OPEN state
+     * 3. The contract has ETH (aka, players)
+     * 4. (Implicit) The subscription is funded with LINK
+     */
+    function checkUpkeep(
+        bytes memory /* checkData */
+    ) public view returns (bool upkeedNeeded, bytes memory /* performData */) {
+        bool timeHasPassed = (block.timestamp - s_lastTimestamp) >= i_interval;
+        bool isOpen = RaffleState.OPEN == s_raffleState;
+        bool hasBalance = address(this).balance > 0;
+        bool hasPlayers = s_players.length > 0;
+        upkeedNeeded = (timeHasPassed && isOpen && hasBalance && hasPlayers);
+        return (upkeedNeeded, "0x0");
+    }
+
+    function performUpkeep(bytes calldata /* performData */) external {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) {
+            revert Raffle__UpkeepNotNeeded(
+                s_raffleState,
+                address(this).balance,
+                s_players.length
+            );
+        }
         // Check to see if enough time has passed
         if (block.timestamp + s_lastTimestamp < i_interval) {
             revert();
