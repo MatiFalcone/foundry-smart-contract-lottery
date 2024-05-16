f2d7bad404db861cc9509fe1c5fc111a370da1e3
Matias Falcone <matias.falcone@gmail.com>
2024-05-16 16:42:32 +1000

    WIP - pending to create subscription for vrf

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -7,6 +7,9 @@ import {Test, console} from "forge-std/Test.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 
 contract RaffleTest is Test {
+    /* Events */
+    event EnteredRaffle(address indexed player);
+
     Raffle raffle;
     HelperConfig helperConfig;
     uint256 entranceFee;
@@ -17,22 +20,67 @@ contract RaffleTest is Test {
     uint32 callbackGasLimit;
 
     address public PLAYER = makeAddr("player");
-    uint256 public STARTING_BALANCE = 10 ether;
+    uint256 public constant STARTING_BALANCE = 10 ether;
 
     function setUp() external {
         DeployRaffle deployRaffle = new DeployRaffle();
         (raffle, helperConfig) = deployRaffle.run();
         (
-            uint256 entranceFee,
-            uint256 interval,
-            address vrfCoordinator,
-            bytes32 gasLane,
-            uint64 subscriptionId,
-            uint32 callbackGasLimit
+            entranceFee,
+            interval,
+            vrfCoordinator,
+            gasLane,
+            subscriptionId,
+            callbackGasLimit
         ) = helperConfig.activeNetworkConfig();
+        vm.deal(PLAYER, STARTING_BALANCE);
     }
 
     function testRaffleInitialisesInOpenState() public view {
         assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
+    }
+
+    //////////////////////
+    // enterRaffle      //
+    //////////////////////
+    function testRaffleRevertsWhenYouDontPayEnough() public {
+        // Arrange
+        vm.prank(PLAYER);
+        // Act / Assert
+        vm.expectRevert(Raffle.Raffle__NotEnoughEthSent.selector);
+        raffle.enterRaffle();
+    }
+
+    function testRaffleRecordPlayerWhenTheyEnter() public {
+        // Arrange
+        vm.prank(PLAYER);
+        // Act
+        raffle.enterRaffle{value: entranceFee}();
+        address playerRecorded = raffle.getPlayer(0);
+        // Assert
+        assert(playerRecorded == PLAYER);
+    }
+
+    function testCantEnterWhenRaffleIsCalculating() public {
+        // We have to simulate the conditions for a valid upkeep
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}(); // raffle: hasBalance & isOpen & hasPlayers
+        vm.warp(block.timestamp + interval + 1); // timeHasPassed
+        vm.roll(block.number + 1); // timeHasPassed
+
+        // Create subscription as consumer
+
+        raffle.performUpkeep(""); // valid upkeep
+
+        vm.expectRevert(Raffle.Raffle__RaffleNotOpen.selector); // in the next transaction
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+    }
+
+    function testEmitsEventOnEntrance() public {
+        vm.prank(PLAYER);
+        vm.expectEmit(true, false, false, false, address(raffle));
+        emit EnteredRaffle(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
     }
 }
